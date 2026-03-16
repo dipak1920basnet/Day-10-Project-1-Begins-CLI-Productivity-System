02b61aab458c6cd5500dd0230a8115ec16df754e
dipak1920basnet <dipak1920basnet@gmail.com>
2026-03-16 23:53:17 +0545

    backup the data before saving

--- taskflow/config.py
+++ taskflow/config.py
@@ -1,3 +1,4 @@
 DATA_FILE = "data/tasks.json"
+BACKUP_FILE = "data/tasks_backup.json"
 LOG_FILE = "taskflow.log"
 MAX_TASK_LENGTH = 100
--- taskflow/storage.py
+++ taskflow/storage.py
@@ -1,13 +1,22 @@
 # save/load data
 import json
-from config import DATA_FILE
+from config import DATA_FILE, BACKUP_FILE
+import shutil
 
+
+def backup():
+    shutil.copy(DATA_FILE,BACKUP_FILE)
 
 def save_tasks(tasks):
+    """
+    backup the new data before saving because 
+    if something worng appears while saving we can go back.
+    """ 
+    backup()
 
     with open(DATA_FILE, "w") as f:
         json.dump(tasks, f)
-
+    
 
 def load_tasks():
 
